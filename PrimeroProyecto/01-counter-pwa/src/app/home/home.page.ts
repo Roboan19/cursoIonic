63c7a9a742b959baf52ca73d6b217bdc59d3049a
Roboan19 <41391810+Roboan19@users.noreply.github.com>
2025-11-23 14:42:52 -0500

    primeros pasos en mi curso de ionic

---
+++ PrimeroProyecto/01-counter-pwa/src/app/home/home.page.ts
@@ -0,0 +1,78 @@
+import { PreferencesPlugin } from './../../../node_modules/@capacitor/preferences/dist/esm/definitions.d';
+import { Component } from '@angular/core';
+import { IonHeader, IonContent, IonButton, IonFooter, IonIcon } 
+  from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { chevronDownOutline, chevronUpOutline } from "ionicons/icons";
+import { Preferences } from '@capacitor/preferences';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  imports: [
+    IonHeader,
+    IonContent,
+    IonButton,
+    IonFooter,
+    IonIcon
+],
+})
+export class HomePage {
+
+public num: number = 0;
+public readonly MIN: number = 0;
+public readonly MAX: number = 9999;
+private readonly KEY_NUMBER: string = "ddr_key_number"
+
+  constructor() {
+      addIcons({
+       chevronDownOutline,
+       chevronUpOutline
+      })
+  }
+
+  async ionViewWillEnter(){
+    console.log("ionViewWillEnter")
+
+    const counterPreferences = await Preferences.get({key : this.KEY_NUMBER});
+
+    if (counterPreferences.value){
+      const num = +counterPreferences.value
+
+      if(isNaN(num) || num < this.MIN || num > this.MAX){
+        this.num = this.MIN;
+        this.saveNum();
+      }
+      else 
+      {
+        this.num = num;
+      }
+    }
+  }
+   upClick() {
+        if (this.num < this.MAX ){
+        this.num++;
+        this.saveNum();
+        
+        } 
+         
+     }
+  
+
+   downClick(){
+        if (this.num > this.MIN ){
+        this.num--;
+        this.saveNum();
+        } 
+  }
+
+
+saveNum(){
+ Preferences.set({ key: this.KEY_NUMBER, value: this.num.toString()}) 
+}
+
+
+
+
+}
